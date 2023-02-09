a2a6cb2cc3d3d91536b815760bc3aac0f9ce8738
Xipilscode <120372707+Xipilscode@users.noreply.github.com>
2023-02-08 17:54:19 -0800

    Streamlit 1st upload

---
+++ Capital_Allocation_Optimization/streamlit_front_end_cap_alloc/main_streamlit.py
@@ -0,0 +1,12 @@
+# Import dependencies
+import streamlit as st
+
+# Create containers
+description = st.beta_container()
+cap_allocation = st.beta_container()
+machine_learning = st.beta_container()
+grid_bot = st.beta_container()
+
+# Write in the container 
+with description:
+    st.Title('CAPTS: Comprehensive Allocation and Portfolio Trading Solution')
